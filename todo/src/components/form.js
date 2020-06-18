d36a9b8f283245473ea9fb83641c4ee48503c87b
Julianisrel <52305213+Julianisrel@users.noreply.github.com>
2020-06-17 23:05:12 -0700

    installed React created compontents

---
+++ todo/src/components/form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AddTodo = (props) => {
+    return (  
+        <div>
+    <input 
+        class="form-control" 
+        type="text" 
+        placeholder="Things u gotta do!"></input>
+    </div>
+    );
+}
+ 
+export default AddTodo;
---
+++ todo/src/components/todoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+const TodoList = (props) => {
+    return ( 
+    <ul class="list-group">
+
+    <li class="list-group-item">Cras justo odio</li>
+  
+  </ul> );
+}
+ 
+export default TodoList;
